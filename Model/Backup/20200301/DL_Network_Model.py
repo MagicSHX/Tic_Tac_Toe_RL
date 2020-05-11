53302e6bc360a013336c174746426962d4cc6219
MagicSHX <34002636+MagicSHX@users.noreply.github.com>
2020-05-11 22:51:21 +0800

    fix a defect inside pytorch model

--- Model/Backup/20200301/DL_Network_Model.py
+++ Model/Backup/20200301/DL_Network_Model.py
@@ -3,14 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-#self & init: https://stackoverflow.com/questions/625083/what-init-and-self-do-on-python
     def __init__(self):
-        #super: https://forums.fast.ai/t/why-super-init/20390/2
         super(Net, self).__init__()
         self.fc1 = nn.Linear(9, 27)
-        self.dropout1 = nn.Dropout(p=0.9, inplace=False)
         self.fc2 = nn.Linear(27, 81)
-        #inplace: https://stackoverflow.com/questions/58589128/what-is-the-meaning-of-in-place-in-dropout
         self.dropout2 = nn.Dropout(p=0.8, inplace=False)
         self.fc3 = nn.Linear(81, 27)
         self.dropout3 = nn.Dropout(p=0.8, inplace=False)
@@ -20,14 +16,9 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        #dropout example: https://medium.com/@zhang_yang/scaling-in-neural-network-dropout-layers-with-pytorch-code-example-11436098d426
-        x = self.dropout1(x)
         x = F.relu(self.fc2(x))
-        x = self.dropout2(x)
         x = F.relu(self.fc3(x))
-        x = self.dropout3(x)
         x = F.relu(self.fc4(x))
-        x = self.dropout4(x)
         x = self.fc5(x)
         return x
 
